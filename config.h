11a45d44f457223a4c00db89ef504ea08817f68b
Brian Evans <dudedsy@gmail.com>
2018-05-14 23:37:21 -0700

    initial commit

---
+++ config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Gettext package */
+#define GETTEXT_PACKAGE "unity-greeter"
+
+/* Indicator files are searched for in this directory */
+#define INDICATOR_FILE_DIR "NONE/share/unity/indicators"
+
+/* Name of package */
+#define PACKAGE "unity-greeter"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "unity-greeter"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "unity-greeter 16.04.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "unity-greeter"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "16.04.2"
+
+/* Path to g-s-d */
+#define USD_BINARY "/usr/lib/unity-settings-daemon/unity-settings-daemon"
+
+/* Version number of package */
+#define VERSION "16.04.2"
